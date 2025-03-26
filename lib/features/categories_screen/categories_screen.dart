5f0469237495b3fce13d8c474b4c4ee9d14b194c
arc1o1 <mwesiga1601@gmail.com>
2025-03-26 05:46:26 +0300

    starting_implementation_of_categories_screens

---
+++ lib/features/categories_screen/categories_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key, required this.categoryTitle});
+
+  final String categoryTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
