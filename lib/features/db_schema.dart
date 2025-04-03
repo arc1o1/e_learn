a62912045646c14979ffc19de6ca7fdac1c90f5e
arc1o1 <mwesiga1601@gmail.com>
2025-04-03 05:49:50 +0300

    structure_of_json_response_format

---
+++ lib/features/db_schema.dart
@@ -0,0 +1,149 @@
+final db = {
+  "schools": [
+    {"id": 1, "name": "Nursery"},
+    {"id": 2, "name": "Primary"},
+    {"id": 3, "name": "O-Level"},
+    {"id": 4, "name": "A-Level"},
+    {"id": 5, "name": "Skill-Based"}
+  ],
+  "classes": [
+    {"id": 1, "school_level": "Nursery", "name": "English Medium"},
+    {"id": 2, "school_level": "Nursery", "name": "Swahili Medium"},
+    {"id": 3, "school_level": "Primary", "name": "Standard I"},
+    {"id": 4, "school_level": "Primary", "name": "Standard II"},
+    {"id": 5, "school_level": "Primary", "name": "Standard III"},
+    {"id": 6, "school_level": "Primary", "name": "Standard IV"},
+    {"id": 7, "school_level": "Primary", "name": "Standard VII"},
+    {"id": 8, "school_level": "O-Level", "name": "Form I"},
+    {"id": 9, "school_level": "O-Level", "name": "Form II"},
+    {"id": 10, "school_level": "O-Level", "name": "Form III"},
+    {"id": 11, "school_level": "O-Level", "name": "Form IV"},
+    {"id": 12, "school_level": "A-Level", "name": "Form V"},
+    {"id": 13, "school_level": "A-Level", "name": "Form VI"},
+    {"id": 14, "school_level": "Skill-Based", "name": "Computer Science"},
+    {"id": 15, "school_level": "Skill-Based", "name": "Language"}
+  ],
+  "subjects": [
+    {
+      "id": 1,
+      "class_name": "English Medium",
+      "name": "Language and Communication"
+    },
+    {"id": 2, "class_name": "English Medium", "name": "Early Life Skills"},
+    {"id": 3, "class_name": "English Medium", "name": "Health and Environment"},
+    {"id": 4, "class_name": "English Medium", "name": "Early Arithmetic"},
+    {"id": 5, "class_name": "English Medium", "name": "Relating with Others"},
+    {
+      "id": 6,
+      "class_name": "Swahili Medium",
+      "name": "Language and Communication"
+    },
+    {"id": 7, "class_name": "Swahili Medium", "name": "Early Life Skills"},
+    {"id": 8, "class_name": "Swahili Medium", "name": "Health and Environment"},
+    {"id": 9, "class_name": "Swahili Medium", "name": "Early Arithmetic"},
+    {"id": 10, "class_name": "Swahili Medium", "name": "Relating with Others"},
+    {"id": 11, "class_name": "Standard I", "name": "Arithmetic"},
+    {"id": 12, "class_name": "Standard I", "name": "Writing"},
+    {"id": 13, "class_name": "Standard I", "name": "Reading"},
+    {"id": 14, "class_name": "Standard I", "name": "Health and Environment"},
+    {"id": 15, "class_name": "Standard I", "name": "Sport and Arts"},
+    {"id": 16, "class_name": "Standard II", "name": "Arithmetic"},
+    {"id": 17, "class_name": "Standard II", "name": "Writing"},
+    {"id": 18, "class_name": "Standard II", "name": "Reading"},
+    {"id": 19, "class_name": "Standard II", "name": "Health and Environment"},
+    {"id": 20, "class_name": "Standard II", "name": "Sport and Arts"},
+    {"id": 21, "class_name": "Form I", "name": "History"},
+    {"id": 22, "class_name": "Form I", "name": "Geography"},
+    {"id": 23, "class_name": "Form I", "name": "Physics"},
+    {"id": 24, "class_name": "Form I", "name": "Chemistry"},
+    {"id": 25, "class_name": "Form I", "name": "Biology"},
+    {
+      "id": 26,
+      "class_name": "Computer Science",
+      "name": "Information Technology"
+    },
+    {"id": 27, "class_name": "Computer Science", "name": "Programming"},
+    {
+      "id": 28,
+      "class_name": "Computer Science",
+      "name": "Computer Troubleshooting and Maintainance"
+    },
+    {"id": 29, "class_name": "Computer Science", "name": "System Design"},
+    {"id": 30, "class_name": "Language", "name": "English Course"},
+    {"id": 31, "class_name": "Language", "name": "Spanish Course"},
+    {"id": 32, "class_name": "Language", "name": "French Course"},
+    {"id": 33, "class_name": "Language", "name": "Chinese Course"}
+  ],
+  "topics": [
+    {
+      "id": 1,
+      "school_level": "O-Level",
+      "class_name": "Form I",
+      "subject_name": "History",
+      "name": "Introduction and Importance of History"
+    },
+    {
+      "id": 2,
+      "school_level": "O-Level",
+      "class_name": "Form I",
+      "subject_name": "History",
+      "name": "Ancient Civilizations"
+    },
+    {
+      "id": 3,
+      "school_level": "O-Level",
+      "class_name": "Form I",
+      "subject_name": "Mathematics",
+      "name": "Numbers and Operations"
+    },
+    {
+      "id": 4,
+      "school_level": "O-Level",
+      "class_name": "Form I",
+      "subject_name": "Mathematics",
+      "name": "Algebraic Expressions"
+    },
+    {
+      "id": 5,
+      "school_level": "O-Level",
+      "class_name": "Form II",
+      "subject_name": "Physics",
+      "name": "Measurements and Units"
+    },
+    {
+      "id": 6,
+      "school_level": "O-Level",
+      "class_name": "Form II",
+      "subject_name": "Physics",
+      "name": "Forces and Motion"
+    },
+    {
+      "id": 7,
+      "school_level": "O-Level",
+      "class_name": "Form III",
+      "subject_name": "Chemistry",
+      "name": "Atomic Structure"
+    },
+    {
+      "id": 8,
+      "school_level": "O-Level",
+      "class_name": "Form III",
+      "subject_name": "Chemistry",
+      "name": "Chemical Bonding"
+    },
+    {
+      "id": 9,
+      "school_level": "A-Level",
+      "class_name": "Form V",
+      "subject_name": "Biology",
+      "name": "Cell Structure and Function"
+    },
+    {
+      "id": 10,
+      "school_level": "A-Level",
+      "class_name": "Form VI",
+      "subject_name": "Biology",
+      "name": "Genetics and Heredity"
+    }
+  ]
+};
