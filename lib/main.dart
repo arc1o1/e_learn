638b79fc2fd8f6c747808d8bc0e298e7acf232c3
arc1o1 <mwesiga1601@gmail.com>
2025-04-09 05:43:47 +0300

    added_dio_locator_service_and_profile_view_model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:dio/dio.dart';
-import 'package:e_learn/features/auth/provider/auth_provider.dart';
-import 'package:e_learn/services/auth_service.dart';
+import 'package:e_learn/features/profile/profile_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,6 +6,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
+import 'package:e_learn/features/auth/provider/auth_provider.dart';
 import 'package:e_learn/features/auth/signin_screen/signin_view_model.dart';
 import 'package:e_learn/features/auth/signup_screen/signup_view_model.dart';
 import 'package:e_learn/features/bottom_nav_menu/bottom_nav_menu.dart';
@@ -15,6 +14,8 @@ import 'package:e_learn/features/bottom_nav_menu/notifier/bottom_nav_menu_notifi
 import 'package:e_learn/features/payments/notifier/payment_notifier.dart';
 import 'package:e_learn/features/school_level_category_screen/notifier/school_level_category_notifier.dart';
 import 'package:e_learn/features/topics_screen/notifier/topics_notifier.dart';
+import 'package:e_learn/services/auth_service.dart';
+import 'package:e_learn/services/dio_service_locator.dart';
 import 'package:e_learn/services/environment_service.dart';
 import 'package:e_learn/services/notifications.dart';
 import 'package:e_learn/services/routes.dart';
@@ -33,20 +34,29 @@ void main() async {
   await NotificationServices().initNotification();
 
   //  initialize get storage
-  GetStorage.init();
+  await GetStorage.init();
 
+  // load env file
   await dotenv.load(fileName: EnvironmentService.fileName);
 
   // create Dio instance and pass to AuthService
-  final dio = Dio(BaseOptions(baseUrl: EnvironmentService.appBaseUrl));
-  final authService = AuthService(dio);
+  // final dio = Dio(BaseOptions(baseUrl: EnvironmentService.appBaseUrl));
+  // final authService = AuthService(dio);
+
+  // setup dependency injection
+  setupLocator();
 
   runApp(MultiProvider(
     providers: [
       // auth
-      ChangeNotifierProvider(create: (_) => AuthProvider(authService)),
+      ChangeNotifierProvider(
+        create: (_) => AuthProvider(locator<AuthService>()),
+      ),
       ChangeNotifierProvider(create: (_) => SigninViewModel()),
       ChangeNotifierProvider(create: (_) => SignupViewModel()),
+
+      // profile
+      ChangeNotifierProvider(create: (_) => ProfileViewModel()),
 
       ChangeNotifierProvider(create: (_) => BottomNavigationNotifier()),
       ChangeNotifierProvider(create: (_) => SchoolLevelCategoryNotifier()),
